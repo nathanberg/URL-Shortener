4fb4868d42f1b5dc6d542a643bd4e2c241c25ac4
Nathan Berg <netnathan10@gmail.com>
2021-05-21 12:06:39 -0400

    Got basic specs passing
    Model initially completed, then worked on controller to get specs passing.

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -2,14 +2,47 @@ class ShortUrlsController < ApplicationController
 
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
+  before_action :only_respond_to_json
+  before_action :set_short_url, only: [:show]
+
 
   def index
+    short_urls = ShortUrl.limit(100).order('click_count')
+    render json: { urls: short_urls.map {|short_url| short_url.public_attributes } }, status: 200
   end
 
   def create
+    @short_url = ShortUrl.new(short_url_params)
+
+    if @short_url.save
+      render json: { short_code: @short_url.short_code }, status: :ok
+    else
+      render json: {errors: "Full url is not a valid url"}
+    end
   end
 
   def show
+    if @short_url
+      @short_url.increment!(:click_count)
+      redirect_to @short_url.full_url
+    else
+      render :json => {:error => "not-found"}.to_json, :status => 404
+    end
   end
 
+  private
+
+  def only_respond_to_json
+    head :not_acceptable unless params[:format] == 'json'
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_short_url
+    @short_url = ShortUrl.find_by_short_code(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def short_url_params
+    params.permit(:full_url)
+  end
 end
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,17 +2,47 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
+  validates_presence_of :full_url
   validate :validate_full_url
 
+  # bitjective function was shamelessly stolen from: https://gist.github.com/zumbojo/1073996
   def short_code
+    i = id
+    return nil if i == 0 || i.nil?
+    s = ''
+    base = CHARACTERS.length
+    while i > 0
+      s << CHARACTERS[i.modulo(base)]
+      i /= base
+    end
+    s.reverse
+  end
+
+  def public_attributes
+    self.attributes.slice('full_url', 'short_code')
+  end
+
+  def self.find_by_short_code(short_code)
+    # based on base2dec() in Tcl translation
+    # at http://rosettacode.org/wiki/Non-decimal_radices/Convert#Ruby
+    i = 0
+    base = CHARACTERS.length
+    short_code.each_char { |c| i = i * base + CHARACTERS.index(c) }
+    i
+    ShortUrl.find_by(id: i)
   end
 
   def update_title!
+    self.title = Nokogiri::HTML(Net::HTTP.get(URI(full_url))).at_css('title').text
+    self.save
   end
 
   private
 
   def validate_full_url
+    if full_url.nil? || !full_url.match?(URI::DEFAULT_PARSER.make_regexp(%w(http https)))
+      errors.add(:full_url, 'is not a valid url')
+    end
   end
 
 end
